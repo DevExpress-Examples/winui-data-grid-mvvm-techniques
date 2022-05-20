a4348a274e42015039cb3fa52bd7aa45987be086
AndreySlabov <65009440+AndreySlabov@users.noreply.github.com>
2022-05-20 14:27:58 +0400

    Add files via upload

---
+++ CS/WinUIMVVMGrid/WinUIMVVMGrid/DataModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace WinUIMVVMGrid {
+    public class Product {
+        public string ProductName { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public double UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+    public class ProductsDataModel {
+        public static ObservableCollection<Product> GetProducts() {
+            ObservableCollection<Product> products = new ObservableCollection<Product> {
+                new Product() {
+                    ProductName = "Chang", Country = "UK", City = "Cowes",
+                    UnitPrice = 19, Quantity = 10, OrderDate = new DateTime(2021, 10, 23)
+                },
+                new Product() {
+                    ProductName = "Gravad lax", Country = "Italy", City = "Reggio Emilia",
+                    UnitPrice = 12.5, Quantity = 16, OrderDate = new DateTime(2021, 10, 22)
+                },
+                new Product() {
+                    ProductName = "Ravioli Angelo", Country = "Brazil", City = "Rio de Janeiro",
+                    UnitPrice = 19, Quantity = 12, OrderDate = new DateTime(2021, 10, 21)
+                },
+                new Product() {
+                    ProductName = "Tarte au sucre", Country = "Germany", City = "Leipzig",
+                    UnitPrice = 22, Quantity = 50, OrderDate = new DateTime(2021, 10, 15)
+                },
+                new Product() {
+                    ProductName = "Steeleye Stout", Country = "USA", City = "Denver",
+                    UnitPrice = 18, Quantity = 20, OrderDate = new DateTime(2021, 10, 8)
+                },
+                new Product() {
+                    ProductName = "Pavlova", Country = "Austria", City = "Graz",
+                    UnitPrice = 21, Quantity = 52, OrderDate = new DateTime(2021, 10, 1)
+                },
+                new Product() {
+                    ProductName = "Longlife Tofu", Country = "USA", City = "Boise",
+                    UnitPrice = 7.75, Quantity = 120, OrderDate = new DateTime(2021, 9, 17)
+                },
+                new Product() {
+                    ProductName = "Alice Mutton", Country = "Mexico", City = "México D.F.",
+                    UnitPrice = 21, Quantity = 15, OrderDate = new DateTime(2021, 9, 25)
+                }
+            };
+            return products;
+        }
+    }
+}
---
+++ CS/WinUIMVVMGrid/WinUIMVVMGrid/ViewModel.cs
@@ -0,0 +1,67 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.CodeGenerators;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+
+namespace WinUIMVVMGrid {
+
+    [GenerateViewModel(ImplementISupportUIServices=true)]
+    public partial class MainViewModel {
+        public MainViewModel() {
+            Source = ProductsDataModel.GetProducts();
+
+            IList Countries = Source.Select(x => x.Country).Distinct().ToList();
+            Columns = new ObservableCollection<TextColumn>() {
+                new TextColumn(nameof(Product.ProductName)),
+                new ComboBoxColumn(nameof(Product.Country), Countries),
+                new TextColumn(nameof(Product.UnitPrice)),
+                new DateColumn(nameof(Product.OrderDate))
+            };
+            Selection = new ObservableCollection<Product>() {Source.ElementAt(0)};
+            Selection.CollectionChanged += (s, e) => ShowSelectedRowsCommand.RaiseCanExecuteChanged();
+        }
+
+        [GenerateProperty]
+        ObservableCollection<Product> source;
+
+        [GenerateProperty]
+        ObservableCollection<TextColumn> columns;
+
+        [GenerateProperty]
+        ObservableCollection<Product> selection;
+        IMessageBoxService MessageBoxService => GetUIService<IMessageBoxService>();
+
+        [GenerateCommand]
+        async void ShowSelectedRows() {
+            string Text = "";
+            foreach (Product product in Selection) {
+                Text += product.ProductName + "\r\n";
+            }
+            await MessageBoxService.ShowAsync(Text, "Selected Products");
+        }
+        bool CanShowSelectedRows() => Selection.Any();
+
+        [GenerateCommand]
+        void AddingNewRow(AddingNewEventArgs e) =>
+            e.NewObject = new Product {ProductName = "", Country = "Austria", UnitPrice = 0, OrderDate = System.DateTime.Today};
+    }
+
+
+    public class TextColumn : BindableBase {
+        public TextColumn(string fieldname) {
+            FieldName = fieldname;
+        }
+        public string FieldName { get; }
+    }
+    public class ComboBoxColumn : TextColumn {
+        public ComboBoxColumn(string fieldname, IList items) : base(fieldname) {
+            Items = items;
+        }
+        public IList Items { get; }
+    }
+    public class DateColumn : TextColumn {
+        public DateColumn(string fieldname) : base(fieldname) { }
+    }
+}
